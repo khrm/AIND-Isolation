3a41dc1416d91fb5d73ef8024789eb57797cd130
khrm <khrm {dot} baig AtRate gmail {dot} com>
2017-02-20 04:51:59 +0530

    Added research review

--- tournament.py
+++ tournament.py
@@ -34,7 +34,7 @@ from game_agent import CustomPlayer
 from game_agent import custom_score
 import game_agent
 
-NUM_MATCHES = 10  # number of matches against each opponent
+NUM_MATCHES = 5  # number of matches against each opponent
 TIME_LIMIT = 150  # number of milliseconds before timeout
 
 TIMEOUT_WARNING = "One or more agents lost a match this round due to " + \
